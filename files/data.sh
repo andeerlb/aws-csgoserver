1a3ed314015f4b6f93a12fcbedac484558f20182
andeerlb <andeerlb@gmail.com>
2021-09-22 17:54:27 -0300

    configuracao de crontab

--- files/data.sh
+++ files/data.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+USER="ec2-user"
+
 yum update -y
 amazon-linux-extras install epel -y
 yum install curl wget tar bzip2 gzip unzip python3 binutils bc jq tmux glibc.i686 libstdc++ libstdc++.i686 -y
 
-wget -O /home/ec2-user/linuxgsm.sh https://linuxgsm.sh 
-chmod +x /home/ec2-user/linuxgsm.sh
+wget -O /home/$USER/linuxgsm.sh https://linuxgsm.sh 
+chmod +x /home/$USER/linuxgsm.sh
 
-cd /home/ec2-user/
+cd /home/$USER/
 
-su ec2-user -s ./linuxgsm.sh csgoserver
+su $USER -s ./linuxgsm.sh csgoserver
 
-su ec2-user -s ./csgoserver auto-install
-echo gslt=\"13D2E94677FC1C3859AF60FAFBE17234\" >> /home/ec2-user/lgsm/config-lgsm/csgoserver/csgoserver.cfg
+su $USER -s ./csgoserver auto-install
+echo gslt=\"13D2E94677FC1C3859AF60FAFBE17234\" >> /home/$USER/lgsm/config-lgsm/csgoserver/csgoserver.cfg
 
 crontab -l > csgoserver_update
 crontab -l > csgoserver_monitor
 
-echo "0 * * * * su - ec2-user -c '/home/ec2-user/lgsm update'" > csgoserver_update
-echo "*/5 * * * *  su - username -c '/home/username/gameserver monitor'" > csgoserver_monitor
+echo "0 * * * * su - $USER -c '/home/$USER/lgsm update'" > csgoserver_update
+echo "*/5 * * * *  su - $USER -c '/home/$USER/csgoserver monitor'" > csgoserver_monitor
 
 crontab csgoserver_update
 crontab csgoserver_monitor
@@ -26,5 +28,7 @@ crontab csgoserver_monitor
 rm csgoserver_updateecho
 rm csgoserver_monitor
 
-su ec2-user -s ./csgoserver update
-su ec2-user -s ./csgoserver start
+su $USER -s ./csgoserver update
+su $USER -s ./csgoserver start
+
+yum update -y
