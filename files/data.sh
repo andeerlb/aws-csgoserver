724c2d95615a2d705c0a14458e20a2137b611f47
andeerlb <andeerlb@gmail.com>
2021-09-22 18:24:56 -0300

    user

--- files/data.sh
+++ files/data.sh
@@ -16,17 +16,17 @@ su $USER -s ./linuxgsm.sh csgoserver
 su $USER -s ./csgoserver auto-install
 echo gslt=\"13D2E94677FC1C3859AF60FAFBE17234\" >> /home/$USER/lgsm/config-lgsm/csgoserver/csgoserver.cfg
 
-crontab -l > csgoserver_update
-crontab -l > csgoserver_monitor
+su $USER -s crontab -l > /home/$USER/csgoserver_update
+su $USER -s crontab -l > /home/$USER/csgoserver_monitor
 
-echo "0 * * * * su - $USER -c '/home/$USER/lgsm update'" > csgoserver_update
-echo "*/5 * * * *  su - $USER -c '/home/$USER/csgoserver monitor'" > csgoserver_monitor
+echo "0 * * * * su - $USER -c '/home/$USER/lgsm update'" > /home/$USER/csgoserver_update
+echo "*/5 * * * *  su - $USER -c '/home/$USER/csgoserver monitor'" > /home/$USER/csgoserver_monitor
 
-crontab csgoserver_update
-crontab csgoserver_monitor
+crontab /home/$USER/csgoserver_update
+crontab /home/$USER/csgoserver_monitor
 
-rm csgoserver_updateecho
-rm csgoserver_monitor
+rm /home/$USER/csgoserver_updateecho
+rm /home/$USER/csgoserver_monitor
 
 su $USER -s ./csgoserver update
 su $USER -s ./csgoserver start
